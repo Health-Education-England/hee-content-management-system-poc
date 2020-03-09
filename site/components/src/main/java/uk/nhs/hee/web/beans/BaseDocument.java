347bce112cc9942bf9fdaeb6ec76a9006adb84f3
Pat Shone <pat.shone@manifesto.co.uk>
2020-03-09 16:41:11 +0000

    feature: first commit of BloomReach archetype created project

---
+++ site/components/src/main/java/uk/nhs/hee/web/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package uk.nhs.hee.web.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="heeweb:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
